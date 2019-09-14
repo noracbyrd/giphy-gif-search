b7d4c1a7a39bec12f082a28b787c7e25da8e88d1
noracyrd <norabyrd@alumni.upenn.edu>
2019-09-14 13:34:30 -0500

    added link to profile, added reset css, built button generator

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var topics = ["The Little Mermaid", "Ariel", "Prince Eric", "Sebastian", "Flounder"];
+
+
+
+function renderButtons() {
+    $("#buttonLibrary").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var item = $("<button>");
+        $(item).addClass("subject");
+        $(item).attr("data-topic",topics[i]);
+        $(item).text(topics[i]);
+        $("#buttonLibrary").append(item);
+    }
+}
