aa87a345e456a58e39f5b2aba5b744e23b70fc54
noracyrd <norabyrd@alumni.upenn.edu>
2019-09-16 20:47:36 -0500

    started work on ajax call & added divs in html for search box and gif space

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var topics = ["The Little Mermaid", "Ariel", "Prince Eric", "Sebastian", "Flounder"];
 
-
+$(".button").on("click", function() {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api-key=B512iYGR2yIt5fyZEJzMSAMKwAQbh5xX";
+    $.ajax({
+        url: queryURL,
+        Method: "GET",
+        q: $(this).attr("data-topic"),
+    })
+    .then(function (response){
+        var image = response.fixed_height_still.url;
+        
+    })
+})
 
 function renderButtons() {
     $("#buttonLibrary").empty();
